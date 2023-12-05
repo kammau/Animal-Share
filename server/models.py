a1ca68ce65ea9a1f77543546361a8194c3ceb337
Kamryn Smith <programersign@gmail.com>
2023-12-05 10:40:22 -0800

    Added basic structure to Animal model

--- server/models.py
+++ server/models.py
@@ -10,12 +10,29 @@ class User(db.Model, SerilaizerMixin):
     username = db.Column(db.String, unique=True, nullable=False)
     accountType = db.Column(db.String)
 
+    #Add __repr__
 
 class Animal(db.Model, SerilaizerMixin):
-    pass
+    __tablename__ = "animals"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String, nullable=False)
+    breed = db.Column(db.String)
+    age = db.Column(db.Integer)
+    location = db.Column(db.String)
+    sex = db.Column(db.String, nullable=False)
+
+
+    #Add __repr__
+    
 
 class Message(db.Model, SerilaizerMixin):
     pass
 
+    #Add __repr__
+
 class Post(db.Model, SerilaizerMixin):
     pass
+
+    #Add __repr__
