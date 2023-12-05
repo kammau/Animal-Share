af33e882bf357f668faa119199d28edc40e204a6
Kamryn Smith <programersign@gmail.com>
2023-12-05 10:30:54 -0800

    created classes for each model

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,40 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import {createStore} from "redux";
 
-//STORE
+//STORE -> GLOBALIZED STATE
+
+//ACTION INCREMENT
+const increment = () => {
+    return {
+        type: "INCREMENT"
+    }
+}
+
+const decrement = () => {
+    return {
+        type: "DECREMENT"
+    }
+}
+
+//REDUCER
+const counter = (state = 0, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return state + 1;
+        case "DECREMENT":
+            return state - 1;
+    }
+}
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH
+store.dispatch(increment());
 
 const container = document.getElementById("root");
 const root = createRoot(container)
--- server/models.py
+++ server/models.py
@@ -2,3 +2,16 @@ from sqlalchemy_serializer import SerilaizerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
+
+class User(db.Model, SerilaizerMixin):
+    pass
+
+
+class Animal(db.Model, SerilaizerMixin):
+    pass
+
+class Message(db.Model, SerilaizerMixin):
+    pass
+
+class Post(db.Model, SerilaizerMixin):
+    pass
