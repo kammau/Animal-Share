f194829df346eb7a12c07e0b499f9371e9e9fafb
Kamryn Smith <programersign@gmail.com>
2023-12-13 13:00:18 -0800

    added store to be accessible globally in index js

--- client/src/index.js
+++ client/src/index.js
@@ -1,42 +1,15 @@
-import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
-import { createRoot } from "react-dom/client";
-import {createStore} from "redux";
+import { Provider } from "react-redux";
+import { store } from "./state/store.ts";
 
-//STORE -> GLOBALIZED STATE
+// const container = document.getElementById("root");
+// const root = createRoot(container)
+// root.render(<App />)
 
-//ACTION INCREMENT
-const increment = () => {
-    return {
-        type: "INCREMENT"
-    }
-}
-
-const decrement = () => {
-    return {
-        type: "DECREMENT"
-    }
-}
-
-//REDUCER
-const counter = (state = 0, action) => {
-    switch (action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-    }
-}
-
-let store = createStore(counter);
-
-//Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-//DISPATCH
-store.dispatch(increment());
-
-const container = document.getElementById("root");
-const root = createRoot(container)
-root.render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
