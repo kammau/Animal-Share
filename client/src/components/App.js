977a5ed62036440b4aaf6d505392ad2ba19ee1b0
Kamryn Smith <programersign@gmail.com>
2024-01-02 14:04:18 -0800

    removed provider from app js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "../redux/store";
+
 
 import Login from "../features/users/Login";
 
 function App() {
     return (
-        <Provider store={store}>
-            <div>
-                <Login />
-            </div>
-        </Provider>
+        <div>
+            <Login />
+        </div>
     )
 }
 
