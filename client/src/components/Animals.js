4007651206163ea89edcbb65b755d4f20c2d726d
Kamryn Smith <programersign@gmail.com>
2024-01-16 13:44:41 -0800

    adding basic layout to animals component

--- client/src/components/Animals.js
+++ client/src/components/Animals.js
@@ -1,15 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Animals() {
+    // Maybe in future change to hold in redux state
+    const [animals, setAnimals] = useState()
 
     useEffect(() => {
         fetch("/animals")
         .then((res) => res.json())
-        .then((res) => console.log(res))
+        .then((res) => setAnimals(res))
     }, [])
     
     return (
-        <h1>Animals!</h1>
+        <div id="animals_body">
+            <h1>Animals!</h1>
+            <div id="animal_card_container">
+                {animals ? animals.map((animal) => {
+                    return (
+                        <div className="animal_card" key={animal.id}>
+                            <h1>{animal.name}</h1>
+                        </div>
+                    )
+                }) : <h1>Error!</h1>}
+            </div>
+        </div>
     )
 }
 
