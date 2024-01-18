0bd76f961d95a70328c407a7047a2197b937e366
Kamryn Smith <programersign@gmail.com>
2024-01-18 10:32:39 -0800

    added basic html outline for new animal form

--- client/src/components/Animals.js
+++ client/src/components/Animals.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 function Animals() {
     // Maybe in future change to hold in redux state
     const [animals, setAnimals] = useState()
+    const [addBtn, setAddBtn] = useState(false)
 
     useEffect(() => {
         fetch("/animals")
@@ -12,6 +15,41 @@ function Animals() {
     
     return (
         <div id="animals_body">
+            {addBtn === true ? (
+                <>
+                    <button id="add_animal_btn" onClick={() => setAddBtn(false)}>-</button>
+                    <form>
+                        <label htmlFor="name">Name</label>
+                        <input name="name" type="text" />
+
+                        <label htmlFor="age">Age</label>
+                        <input name="age" type="number" />
+
+                        <label htmlFor="breed">Breed</label>
+                        <input name="breed" type="text" />
+
+                        <label htmlFor="species">Species</label>
+                        <input name="species" type="text" />
+
+                        <label htmlFor="location">Location</label>
+                        <input name="location" type="text" />
+
+                        <label for="sex_m">M</label>
+                        <input type="radio" id="sex_m" name="sex" />
+                        
+                        <label for="sex_f">F</label>
+                        <input type="radio" id="sex_f" name="sex" />
+
+                        <label htmlFor="img">Picture</label>
+                        <input type="text" name="img" />
+
+                        <button type="submit">Add Animal</button>
+                    </form>
+                </>
+            ) : (
+                <button id="add_animal_btn" onClick={() => setAddBtn(true)}>+</button>
+            )}
+            
             <div id="animal_cards_container">
                 {animals ? animals.map((animal) => {
                     return (
