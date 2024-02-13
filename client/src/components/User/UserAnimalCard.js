d57d75d9ed2264325e895e572fe435fb25207933
Kamryn Smith <programersign@gmail.com>
2024-02-12 16:20:21 -0800

    added validation for my animals edit

--- client/src/components/User/UserAnimalCard.js
+++ client/src/components/User/UserAnimalCard.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 import { useFormik } from "formik";
+import * as yup from "yup";
 
 
 function UserAnimalCard({animal, handleUpdate, handleDelete}) {
     const [mode, setMode] = useState("view")
+
+    const formSchema = yup.object().shape({
+        name: yup.string().required("Please enter your animals name"),
+        age: yup.number().required("Please enter your animals age"),
+        breed: yup.string().required("Please enter your animals breed"), // Maybe add if unknown breed message
+        species: yup.string().required("Please enter your animals species"),
+        location: yup.string(),
+        sex: yup.string(),
+        img: yup.string().required("Please enter a picture of your animal"),
+        bio: yup.string().required("Please enter some basic information about your animal (25-150)").min(25, "Please enter at least 25 characters").max(150, "Please enter no more than 150 characters")
+    })
 
     const formik = useFormik({
         initialValues: {
@@ -13,8 +25,10 @@ function UserAnimalCard({animal, handleUpdate, handleDelete}) {
             sex: animal.sex,
             age: animal.age,
             location: animal.location,
-            img: animal.img
+            img: animal.img,
+            bio: animal.bio
         },
+        validationSchema: formSchema,
         onSubmit: (values) => {
             fetch(`/my_account/animals/${animal.id}`, {
                 method: "PATCH",
@@ -55,10 +69,11 @@ function UserAnimalCard({animal, handleUpdate, handleDelete}) {
                     <form onSubmit={formik.handleSubmit}>
                         <label htmlFor="name">Name:</label>
                         <input type="text" value={formik.values.name} onChange={formik.handleChange} name="name" className="forms_inputs"/>
+                        <p className="form_errors">{formik.errors.name}</p>
 
                         <label htmlFor="species">Species:</label>
                         <input type="text" value={formik.values.species} onChange={formik.handleChange} name="species" className="forms_inputs"/>
-
+                        <p className="form_errors">{formik.errors.species}</p>
 
                         <div value={formik.values.sex} onChange={formik.handleChange}>
                             <label htmlFor="sex_m_">Male</label>
@@ -66,16 +81,23 @@ function UserAnimalCard({animal, handleUpdate, handleDelete}) {
 
                             <label htmlFor="sex_f_">Female</label>
                             <input type="radio" id="sex_f_" name="sex" value="Female" />
+                            <p className="form_errors">{formik.errors.sex}</p>
                         </div>
 
                         <label htmlFor="age">Age:</label>
                         <input type="number" value={formik.values.age} onChange={formik.handleChange} name="age" className="forms_inputs"/>
+                        <p className="form_errors">{formik.errors.age}</p>
 
                         <label htmlFor="location">Location:</label>
                         <input type="text" value={formik.values.location} onChange={formik.handleChange} name="location" className="forms_inputs"/>
+                        <p className="form_errors">{formik.errors.location}</p>
 
                         <label htmlFor="img">Image:</label>
                         <input type="text" value={formik.values.img} onChange={formik.handleChange} name="img" className="forms_inputs"/>
+                        <p className="form_errors">{formik.errors.img}</p>
+                        
+                        <input type="text" value={formik.values.bio} onChange={formik.handleChange} name="bio" className="forms_inputs" />
+                        <p className="form_errors">{formik.errors.bio}</p>
 
                         <button type="submit" className="update_btn">Update</button>
                     </form>
